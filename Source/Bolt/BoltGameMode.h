d620a354f2a175585f24c89d88c38f4e1e70e171
BrunoWilkinson <bruno.wilkinson@gmail.com>
2022-07-28 11:14:08 +0100

    Setup

---
+++ Source/Bolt/BoltGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BoltGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ABoltGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ABoltGameMode();
+};
+
+
+
