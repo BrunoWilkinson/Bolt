bc830aa826a6e3ee5310f473049ecff566d4c1de
BrunoWilkinson <bruno.wilkinson@gmail.com>
2022-08-03 18:29:06 +0100

    feat: health component

---
+++ Source/Bolt/HealthComponent.h
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "HealthComponent.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class BOLT_API UHealthComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UHealthComponent();
+
+public:	
+	UFUNCTION(BlueprintPure)
+	float GetHealth() const { return Health; }
+
+	UFUNCTION(BlueprintPure)
+	bool IsDead() const;
+
+	void ApplyDamage(float Value);
+
+private:
+	UPROPERTY(EditAnywhere, Category = Values)
+	float MaxHealth;
+
+	UPROPERTY(VisibleAnywhere, Category = Values)
+	float Health;
+};
